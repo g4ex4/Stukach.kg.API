76318c99b19b2eae9d967f51c79c2d3290598934
Batya <127815363+laravelkg@users.noreply.github.com>
2023-06-17 18:36:01 +0600

    exception

--- WebApi/Middleware/CustomExceptionHandlerMiddleware.cs
+++ WebApi/Middleware/CustomExceptionHandlerMiddleware.cs
@@ -9,35 +9,65 @@ namespace WebApi.Middleware
     {
         private readonly RequestDelegate _next;
 
-        public CustomExceptionHandlerMiddleware(RequestDelegate next) =>
-            _next = next;
+    public CustomExceptionHandlerMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
 
-        public async Task Invoke(HttpContext context)
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
         {
-            try
-            {
-                await _next(context);
-            }
-            catch (Exception exception)
-            {
-                await HandleExceptionAsync(context, exception);
-            }
+            await _next(context);
         }
-
-        private Task HandleExceptionAsync(HttpContext context, Exception exception)
+        catch (CustomException ex)
         {
-            var code = HttpStatusCode.InternalServerError;
-            var result = string.Empty;
-            context.Response.ContentType = "application/json";
-            context.Response.StatusCode = (int)code;
-
-            if (result == string.Empty)
-            {
-                result = JsonSerializer.Serialize(new { error = exception.Message });
-            }
-
-            return context.Response.WriteAsync(new Response
-                (context.Response.StatusCode, result, false).ToString());
+            // Обработка вашего пользовательского исключения
+            await HandleCustomExceptionAsync(context, ex);
         }
+        catch (Exception ex)
+        {
+            // Обработка других типов исключений
+            await HandleExceptionAsync(context, ex);
+        }
+    }
+
+    private static async Task HandleCustomExceptionAsync(HttpContext context, CustomException exception)
+    {
+        // Формирование ответа с информацией об исключении
+        var response = new Response
+        {
+            StatusCode = (int)HttpStatusCode.InternalServerError,
+            Message = exception.Message,
+            IsSuccess = false
+        };
+
+        // Преобразование объекта Response в JSON
+        var jsonResponse = JsonSerializer.Serialize(response);
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = response.StatusCode;
+
+        // Отправка JSON-ответа клиенту
+        await context.Response.WriteAsync(jsonResponse);
+    }
+
+    private static async Task HandleExceptionAsync(HttpContext context, Exception exception)
+    {
+        // Формирование ответа с информацией об исключении
+        var response = new Response
+        {
+            StatusCode = 200,
+            Message = "Произошла ошибка на сервере.",
+            IsSuccess = false
+        };
+
+        // Преобразование объекта Response в JSON
+        var jsonResponse = JsonSerializer.Serialize(response);
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = response.StatusCode;
+
+        // Отправка JSON-ответа клиенту
+        await context.Response.WriteAsync(jsonResponse);
+    }
     }
 }
